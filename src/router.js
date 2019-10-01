cdb56eae5c7eeda556fb7ed71edfc5bb1dfdbdee
Rock <124841882@qq.com>
2019-10-01 09:22:48 +0800

    hello world

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-30 08:46:11
+ * @LastEditTime: 2019-09-30 08:46:11
+ * @LastEditors: your name
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
 
 Vue.use(Router)
 
@@ -8,18 +15,6 @@ export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
+
   ]
 })
